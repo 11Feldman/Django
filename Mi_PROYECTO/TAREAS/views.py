e984b57cefecd705929975862f2912cb784c3180
11Feldman <44484762+11Feldman@users.noreply.github.com>
2021-06-16 21:43:29 -0300

    update files

--- Mi_PROYECTO/TAREAS/views.py
+++ Mi_PROYECTO/TAREAS/views.py
@@ -4,36 +4,32 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-tareas = ['Despertarse', 'Desayunar', 'Lanzar Robot', 'Banarse','Cocinar']
+#tareas = ['Despertarse', 'Desayunar', 'Lanzar Robot', 'Banarse','Cocinar']
 
-
-# Create your views here.
-class NewTasks(forms.Form):
+# Usando session para tomar los datos.
+class NewTask(forms.Form):
     tarea = forms.CharField(label='')
 
 def index(request):
+    if 'tareas' not in request.session:
+        request.session['tareas'] = []
     return render(request, 'tasks/index.html', {
-        'tasks': tareas,
+        'tasks': request.session['tareas'],
         'typeTask': 'tareas'
     })
 
 def add(request):
-    #Chequear si el metodo en la vista es POST
     if request.method == 'POST':
-        #Tomar los datos que el usuario ha enviado y guardarlo como un formulario
-        form = NewTasks(request.POST)
-        #Chequear si el formulario es valido desde el lado del servidor
+        form = NewTask(request.POST)
         if form.is_valid():
             tarea = form.cleaned_data['tarea']
-            tareas.append(tarea)
-            #Redireccionar al usuario al indice
+            request.session['tareas'] += [tarea]
             return HttpResponseRedirect(reverse('TAREAS:index'))
         else:
-            #Si el formulario es invalido, volver a renderizar la pagina web con la informacion existente
             return render(request, 'tasks/add.html',{
-                "formulario_de_alta": form
+                'formulario_de_alta': form
             })
     else:
         return render(request, 'tasks/add.html', {
-            "formulario_de_alta": NewTasks()
+            "formulario_de_alta": NewTask()
         })
