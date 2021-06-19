29f31776f78f114a2bc548b34e681b64d05f9871
11Feldman <44484762+11Feldman@users.noreply.github.com>
2021-06-19 13:21:36 -0300

    Upload files to the project

--- Mi_PROYECTO/TAREAS/views.py
+++ Mi_PROYECTO/TAREAS/views.py
@@ -4,30 +4,33 @@ from django import forms
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 
-# Usando session para tomar los datos.
-class NewTask(forms.Form):
+
+
+# Create your views here.
+class NewTasks(forms.Form):
     tarea = forms.CharField(label='')
 
 def index(request):
-    if 'tareas' not in request.session:
-        request.session['tareas'] = []
+    # Chequear si ya existe una clave “tareas” en nuestra sesion
+    if "tareas" not in request.session:
+        request.session["tareas"] = []
     return render(request, 'tasks/index.html', {
-        'tasks': request.session['tareas'],
+        'tasks': request.session["tareas"],
         'typeTask': 'tareas'
     })
 
 def add(request):
     if request.method == 'POST':
-        form = NewTask(request.POST)
+        form = NewTasks(request.POST)
         if form.is_valid():
             tarea = form.cleaned_data['tarea']
-            request.session['tareas'] += [tarea]
+            request.session["tareas"]+= [tarea]
             return HttpResponseRedirect(reverse('TAREAS:index'))
         else:
             return render(request, 'tasks/add.html',{
-                'formulario_de_alta': form
+                "formulario_de_alta": form
             })
     else:
         return render(request, 'tasks/add.html', {
-            "formulario_de_alta": NewTask()
+            "formulario_de_alta": NewTasks()
         })
---
+++ README.md
@@ -0,0 +1,5 @@
+# Django with Python, HTML5, CSS, JS
+Practice using the django framework with python, html, css and javascript.
+
+
+I am doing the first practices with the django framework.
